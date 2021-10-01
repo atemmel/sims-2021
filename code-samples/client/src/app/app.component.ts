42fdbc044f9bf8979c282ce54d7df3b0e4f5a6aa
AlbinOdelstav <albin.odelstav@outlook.com>
2021-10-01 11:51:22 +0200

    Input field is now cleared when the user clicks enter or send button

--- code-samples/client/src/app/app.component.ts
+++ code-samples/client/src/app/app.component.ts
@@ -22,7 +22,22 @@ export class AppComponent implements OnInit {
 
   // Called when the user clicks on the send button in app.component.html.
   // Sends the users input to the server
-  sendMessage(data: string) {
-    this.webSocketService.emit('event', data);
+  sendMessage(input: any) {
+
+    // Remove whitespace before and after the input string
+    let text = input.value.trim();
+
+    // Do nothing if the input field is empty when the user
+    // clicks on enter or the send button
+    if (text === "") {
+      text = "";
+      return;
+    };
+
+    // Send the string to the middleend
+    this.webSocketService.emit('event', text);
+
+    // Clear the input field
+    input.value = "";
   }
 }
