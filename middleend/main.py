2482b14bc187bc076abde3ad83548617ce990566
atemmel <temmeladam@gmail.com>
2021-10-01 10:18:20 +0200

    Potentially fixed the timeout bug

--- middleend/main.py
+++ middleend/main.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 import eventlet
-import socketio
-
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-from ibm_watson import AssistantV2
-from ibm_watson import ApiException
+from ibm_watson import ApiException, AssistantV2
 import json
+import socketio
+from threading import Lock
+from time import sleep
 
-from requests.models import ContentDecodingError
 sio = socketio.Server(cors_allowed_origins='*')
 app = socketio.WSGIApp(sio)
-articles, assistant, session_id, auth = None, None, None, None
+articles, assistant, auth = None, None, None
+clients_session = {}
+clients_lock = Lock()
 
 def do_repl(articles, assistant, session_id, auth):
     while True:
@@ -32,11 +33,20 @@ def do_repl(articles, assistant, session_id, auth):
             print("\nExiting...")
             break
 
+def connect_client(auth, assistant, client):
+    response = create_session(auth, assistant)
+    if not create_session_succeeded(response):
+        return False
+    clients_lock.acquire()
+    clients_session[client] = response["session_id"]
+    clients_lock.release()
+    return True
 
-
-
-
-
+def disconnect_client(auth, assistant, client):
+    clients_lock.acquire()
+    delete_session(assistant, clients_session[client], auth)
+    del clients_session[client]
+    clients_lock.release()
 
 def read_json_to_dict(string):
     with open(string, "r", encoding="UTF-8") as file:
@@ -55,7 +65,7 @@ def send_message(assistant, session_id, config, message):
 
     return response
 
-def create_session(auth):
+def create_assistant(auth):
     authenticator = IAMAuthenticator(auth["authenticator"])
     assistant = AssistantV2(
         version='2021-06-14',
@@ -63,12 +73,15 @@ def create_session(auth):
     )
 
     assistant.set_service_url(auth["service_url"])
+    return assistant
 
+
+def create_session(auth, assistant):
     response = assistant.create_session(
         assistant_id = auth["assistant_id"]
     ).get_result()
 
-    return assistant, response
+    return response
 
 def delete_session(assistant, session_id, auth):
     assistant.delete_session(
@@ -101,7 +114,6 @@ def find_article_category(articles, category):
         if article["company-field"] == category:
             foundArticles.append(article)
     return foundArticles
-
 
 def replace_entities_in_response(response, articles):
     message = response["output"]["generic"][0]["text"]
@@ -139,15 +151,22 @@ def get_articles_and_urls(response, articles):
     return res
 
 @sio.on('connect')
-def connect(sid, env):
-    print('connecting ', sid)
+def connect(sid, _):
+    print('connecting', sid)
+    # Returns true/false, should perhaps be handled(?)
+    connect_client(auth, assistant, sid)
 
 @sio.on('disconnect')
 def disconnect(sid):
-    print('disconnect ', sid)
+    print('disconnect', sid)
+    disconnect_client(auth, assistant, sid)
 
 @sio.on('event')
 def message(sid, data):
+    clients_lock.acquire()
+    session_id = clients_session[sid]
+    clients_lock.release()
+
     response = send_message(assistant, session_id, auth, data)
     print(response)
     if not send_message_succeeded(response):
@@ -165,24 +184,20 @@ def message(sid, data):
 
 
 def main():
-    global articles,assistant, session_id, auth
+    global articles, assistant, auth
     port = 80
     auth = read_json_to_dict("./auth.json")
     config = read_json_to_dict("./config.json")
     articles = load_articles(config)
     
+    assistant = create_assistant(auth)
     try:
-        assistant, response = create_session(auth)
-        if not create_session_succeeded(response):
-            print("Could not create session:")
-            print(json.dumps(response, indent=2))
-            return 1
-
-        session_id = response["session_id"]
         eventlet.wsgi.server(eventlet.listen(('', port)), app)
 
-
-        delete_session(assistant, session_id, auth)
+        # Cleanup
+        sleep(1)
+        for session in clients_session:
+            delete_session(assistant, session, auth)
     except ApiException as ex:
         print("Method failed with status code", str(ex.code) , ":" , ex.message)
 
