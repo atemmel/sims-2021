f349c57850aa00e6aa3cda2979288d52d30cbd4d
atemmel <temmeladam@gmail.com>
2021-10-28 17:42:16 +0200

    Presentation tweaks

--- middleend/main.py
+++ middleend/main.py
@@ -233,11 +233,24 @@ def generate_response(response, articles):
         }
     ]
 
+    backend_names = [entity["backend_name"] for entity in entities_relevant_to_articles]
+    num_article_relevant_entities = len([entity for entity in entities if entity["entity"] in backend_names])
+    if num_article_relevant_entities == len(entities):
+        # Discrad intents if entities solely discusses articles
+        intents = []
+
+    has_article_entities = num_article_relevant_entities > 0
+    has_article_irrelevant_entities = len(entities) - num_article_relevant_entities > 0
+    has_article_help_intent = "HelpWithArticles" in [intent["intent"] for intent in intents]
+
+    if has_article_help_intent and has_article_entities and has_article_irrelevant_entities:
+        entities = [entity for entity in entities if entity["entity"] in backend_names]
+
     num_intents = len(intents)
     num_unique_entities = len(set([entity["entity"] for entity in entities]))
 
     # Incomprehensible
-    if num_intents >= 1 and num_unique_entities >= 2 or num_unique_entities >= 3:
+    if num_intents >= 1 and num_unique_entities > 2 or num_unique_entities > 3:
         return [{
             "text": "I didn't understand. You can try rephrasing.",
         }]
